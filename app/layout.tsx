ccca3199ace974f6a80dc2d57b927c644b637349
Roman Farooq <theromanfarooq@gmail.com>
2023-10-02 08:29:00 +0500

    added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
           <Toaster position="top-right" reverseOrder={false} />
         </ActiveSectionContextProvider>
       </body>
---
+++ components/footer.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Footer() {
+  return (
+    <motion.footer
+      className="mb-10 px-4 text-center text-gray-500"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+    >
+      <small className="mb-2 block text-xs">
+        &copy; 2023 Roman. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> built with
+        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Hot Toast, Zod, React Email & Resend, and Vercel
+        hosting.
+      </p>
+    </motion.footer>
+  );
+}
