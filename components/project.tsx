3b389ec88c1851dc2600491a08427dc24346ea6b
Roman Farooq <theromanfarooq@gmail.com>
2025-04-19 18:52:52 +0500

    add VanillaTilt to projects

--- components/project.tsx
+++ components/project.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
-import { useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
+import { useRef, useEffect } from "react";
 import { projectsData } from "@/lib/data";
-import { motion, useScroll, useTransform } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["0 1", "1.33 1"],
-  });
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  useEffect(() => {
+    if (ref.current) {
+      VanillaTilt.init(ref.current, {
+        max: 5,
+        speed: 400,
+        glare: true,
+        scale: 1.02,
+        gyroscope: false,
+        "max-glare": 0.2,
+      });
+    }
+  }, []);
+
   return (
-    <motion.div
-      ref={ref}
-      style={{
-        scale: scaleProgress,
-        opacity: opacityProgress,
-      }}
-      className="group mb-3 last:mb-0 sm:mb-8"
-    >
-      <section className="relative mx-auto flex flex-col overflow-hidden rounded-lg border border-black/5 bg-gray-100 hover:bg-gray-200 lg:h-72 dark:bg-white/10 dark:text-white dark:hover:bg-white/20">
+    <div ref={ref} className="group mx-8 mb-3 last:mb-0 sm:mb-8">
+      <section className="relative mx-auto flex flex-col overflow-hidden rounded-lg border border-black/5 bg-gray-200 lg:h-72 dark:bg-white/10 dark:text-white">
         <div className="flex flex-col justify-center px-8 py-6 lg:h-full lg:max-w-[45%] lg:px-0 lg:py-0 lg:group-odd:pl-8 lg:group-even:ml-[55%] lg:group-even:pr-8">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 text-justify text-sm leading-relaxed text-gray-700 dark:text-white/70">
@@ -43,13 +44,13 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
           <Image
             src={imageUrl}
             alt={title}
-            quality={95}
+            quality={100}
             priority={true}
-            className="mx-auto h-auto w-full rounded-lg object-contain transition lg:absolute lg:top-1/2 lg:w-[50%] lg:-translate-y-1/2 lg:group-odd:right-0 lg:group-even:left-0 lg:group-hover:scale-105 lg:group-hover:-rotate-2 lg:group-hover:group-even:rotate-2"
+            className="mx-auto h-auto w-full rounded-lg object-contain transition lg:absolute lg:top-1/2 lg:w-[50%] lg:-translate-y-1/2 lg:group-odd:right-0 lg:group-even:left-0"
           />
         </div>
       </section>
-    </motion.div>
+    </div>
   );
 }
 
