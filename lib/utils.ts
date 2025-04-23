d8646856dcbf4a1a12eb4457b8652479e779da59
Roman Farooq <theromanfarooq@gmail.com>
2025-04-23 05:36:31 +0500

    Refactor FlipWords component state initialization and improve remapValue function for clarity and maintainability

--- lib/utils.ts
+++ lib/utils.ts
@@ -30,7 +30,8 @@ export function remapValue(
   start2: number,
   end2: number,
 ) {
-  const remapped =
-    ((value - start1) * (end2 - start2)) / (end1 - start1) + start2;
+  const numerator = (value - start1) * (end2 - start2);
+  const denominator = end1 - start1;
+  const remapped = numerator / denominator + start2;
   return remapped > 0 ? remapped : 0;
 }
