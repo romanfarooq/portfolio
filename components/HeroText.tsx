b3902a0c94e81ba78f6c3ad6124641a86a16c3bf
Roman Farooq <TheRomanFarooq@gmail.com>
2025-04-29 02:59:23 +0500

    Refactor layout, HeroText, ProjectDetails, Timeline, About, Contact, Footer, Navbar components for improved styling and functionality; update viewport settings and enhance accessibility.

--- components/HeroText.tsx
+++ components/HeroText.tsx
@@ -11,27 +11,30 @@ const variants = {
 
 export function HeroText() {
   return (
-    <div className="z-10 mt-20 rounded-3xl bg-clip-text text-center md:mt-40 md:text-left">
-      {/* Desktop View */}
-      <div className="hidden flex-col px-5 sm:px-10 md:flex lg:px-15">
+    <div className="z-10 mt-20 rounded-3xl bg-clip-text text-center md:mt-36 md:text-left">
+      <div className="relative flex h-[80vh] flex-col gap-y-5 px-5 sm:px-10 md:flex md:h-auto md:items-start md:justify-start md:px-5 lg:px-15">
         <motion.h1
-          className="text-3xl font-medium"
+          className="text-4xl font-medium md:text-3xl"
           variants={variants}
           initial="hidden"
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I&apos;m Roman
+          Hi, I&apos;m Roman
         </motion.h1>
-        <div className="flex flex-col items-start">
+        <div className="flex w-full flex-col items-center md:items-start">
           <motion.p
-            className="text-5xl font-medium text-neutral-300"
+            className="text-5xl font-medium text-neutral-300 md:text-5xl md:font-medium"
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            A Developer <br /> Dedicated to Crafting
+            <span className="hidden md:inline">
+              A Developer
+              <br /> Dedicated to Crafting
+            </span>
+            <span className="md:hidden">Building</span>
           </motion.p>
           <motion.div
             variants={variants}
@@ -41,11 +44,11 @@ export function HeroText() {
           >
             <FlipWords
               words={words}
-              className="text-8xl font-black text-white"
+              className="text-7xl font-bold text-white md:text-8xl md:font-black"
             />
           </motion.div>
           <motion.p
-            className="text-4xl font-medium text-neutral-300"
+            className="pt-3 text-4xl font-black text-neutral-300 md:text-4xl md:font-medium"
             variants={variants}
             initial="hidden"
             animate="visible"
@@ -53,6 +56,8 @@ export function HeroText() {
           >
             Web Solutions
           </motion.p>
+        </div>
+        <div className="absolute bottom-5 flex w-full justify-center md:static md:justify-start">
           <motion.a
             href="/documents/Roman-Farooq-CV.pdf"
             download
@@ -60,69 +65,7 @@ export function HeroText() {
             initial="hidden"
             animate="visible"
             transition={{ delay: 2 }}
-            className="mt-4 flex items-center gap-2 rounded-full border border-black/10 bg-white/10 px-7 py-2 text-xl outline-hidden transition hover:scale-110 focus-visible:scale-110 active:scale-105"
-          >
-            Download CV
-            <Image
-              src="/assets/icons/arrow-down.svg"
-              alt="Download Icon"
-              width={24}
-              height={24}
-            />
-          </motion.a>
-        </div>
-      </div>
-      {/* Mobile View */}
-      <div className="relative flex h-[80vh] flex-col space-y-6 md:hidden">
-        <motion.p
-          className="text-4xl font-medium"
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          transition={{ delay: 1 }}
-        >
-          Hi, I&apos;m Roman
-        </motion.p>
-        <div>
-          <motion.p
-            className="text-5xl font-black text-neutral-300"
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            transition={{ delay: 1.2 }}
-          >
-            Building
-          </motion.p>
-          <motion.div
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            transition={{ delay: 1.5 }}
-          >
-            <FlipWords
-              words={words}
-              className="text-7xl font-bold text-white"
-            />
-          </motion.div>
-          <motion.p
-            className="text-neutral300 text-4xl font-black"
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            transition={{ delay: 1.8 }}
-          >
-            Web Applications
-          </motion.p>
-        </div>
-        <div className="absolute bottom-5 flex w-full justify-center">
-          <motion.a
-            href="/documents/Roman-Farooq-CV.pdf"
-            download
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            transition={{ delay: 2 }}
-            className="flex items-center gap-2 rounded-full border border-black/10 bg-white px-7 py-2 outline-hidden transition hover:scale-110 focus-visible:scale-110 active:scale-105 dark:bg-white/10"
+            className="flex items-center gap-2 rounded-full border border-black/10 bg-white px-7 py-2 text-xl outline-hidden transition hover:scale-110 focus-visible:scale-110 active:scale-105 md:bg-white/10 md:text-xl dark:bg-white/10"
           >
             Download CV
             <Image
