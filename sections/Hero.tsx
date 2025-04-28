eb44598fd75cd00249e325d453bf3cef8119e0d9
Roman Farooq <TheRomanFarooq@gmail.com>
2025-04-29 00:39:10 +0500

    Refactor Astronaut component and Hero section for improved configuration and rendering; update GLTF model preload settings.

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -9,6 +9,27 @@ import { HeroText } from "@/components/HeroText";
 import { Astronaut } from "@/components/Astronaut";
 import { ParallaxBackground } from "@/components/ParallaxBackground";
 
+type AstronautConfig = {
+  [key in "mobile" | "desktop"]: {
+    scale: number;
+    position: [number, number, number];
+    rotation: [number, number, number];
+  };
+};
+
+const ASTRONAUT_CONFIG: AstronautConfig = {
+  mobile: {
+    scale: 1.2,
+    position: [0.1, -1, 0],
+    rotation: [Math.PI / 6, Math.PI * 0.7, -Math.PI * 0.2],
+  },
+  desktop: {
+    scale: 1.5,
+    position: [1.5, -1, 0],
+    rotation: [Math.PI / 6, Math.PI * 0.7, -Math.PI * 0.2],
+  },
+};
+
 export default function Hero() {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
@@ -23,8 +44,9 @@ export default function Hero() {
           <Suspense fallback={<Loader />}>
             <Float>
               <Astronaut
-                scale={isMobile ? 0.23 : undefined}
-                position={isMobile ? [0, -1.5, 0] : undefined}
+                {...(isMobile
+                  ? ASTRONAUT_CONFIG.mobile
+                  : ASTRONAUT_CONFIG.desktop)}
               />
             </Float>
           </Suspense>
