e2a54f32f60e8885d1b7f777798087cbcbad5bce
Roman Farooq <TheRomanFarooq@gmail.com>
2025-04-28 19:24:54 +0500

    Refactor Astronaut component to simplify skinnedMesh rendering; update @types/node dependency to version 22.15.3; replace multiple image assets with updated versions; clean up Projects section by removing unnecessary line

--- components/Astronaut.tsx
+++ components/Astronaut.tsx
@@ -48,66 +48,26 @@ export function Astronaut(props: ThreeElements["group"]) {
           <group name="Root">
             <group name="metarig">
               <primitive object={nodes.metarig_rootJoint} />
-              <skinnedMesh
-                name="Cube001_0"
-                geometry={(nodes.Cube001_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube001_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube005_0"
-                geometry={(nodes.Cube005_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube005_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube002_0"
-                geometry={(nodes.Cube002_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube002_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Plane_0"
-                geometry={(nodes.Plane_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Plane_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube008_0"
-                geometry={(nodes.Cube008_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube008_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube004_0"
-                geometry={(nodes.Cube004_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube004_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube003_0"
-                geometry={(nodes.Cube003_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube003_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube_0"
-                geometry={(nodes.Cube_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube009_0"
-                geometry={(nodes.Cube009_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube009_0 as SkinnedMesh).skeleton}
-              />
-              <skinnedMesh
-                name="Cube011_0"
-                geometry={(nodes.Cube011_0 as SkinnedMesh).geometry}
-                material={materials["AstronautFallingTexture.png"]}
-                skeleton={(nodes.Cube011_0 as SkinnedMesh).skeleton}
-              />
+              {[
+                "Cube001_0",
+                "Cube005_0",
+                "Cube002_0",
+                "Plane_0",
+                "Cube008_0",
+                "Cube004_0",
+                "Cube003_0",
+                "Cube_0",
+                "Cube009_0",
+                "Cube011_0",
+              ].map((name) => (
+                <skinnedMesh
+                  key={name}
+                  name={name}
+                  geometry={(nodes[name] as SkinnedMesh).geometry}
+                  material={materials["AstronautFallingTexture.png"]}
+                  skeleton={(nodes[name] as SkinnedMesh).skeleton}
+                />
+              ))}
               <group name="Cube001" />
               <group name="Cube005" />
               <group name="Cube002" />
