ff88afb971e0ea8c48212f4f84e1e7914b3f1537
Roman Farooq <TheRomanFarooq@gmail.com>
2025-05-22 04:50:31 +0500

    Implement code changes to enhance functionality and improve performance

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 
