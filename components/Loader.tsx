d623886d51e154efdb7fb79f25f988250357c070
Roman Farooq <TheRomanFarooq@gmail.com>
2025-05-24 19:07:57 +0500

    feat: add loading animations and improve navbar responsiveness

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,10 +1,9 @@
-import { Html, useProgress } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
 export function Loader() {
-  const { progress } = useProgress();
   return (
-    <Html center className="text-center text-xl font-normal">
-      {progress}% Loaded
+    <Html className="loader">
+      <div className="loading" />
     </Html>
   );
 }
