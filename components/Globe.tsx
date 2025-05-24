e6e3448b74d0d7484d37c5a345a00c971cc39c6f
Roman Farooq <TheRomanFarooq@gmail.com>
2025-05-24 20:39:02 +0500

    fix: reorder globe markers for improved organization and clarity

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -20,17 +20,17 @@ const GLOBE_CONFIG: COBEOptions = {
   markerColor: [1, 1, 1],
   glowColor: [1, 1, 1],
   markers: [
-    { location: [14.5995, 120.9842], size: 0.03 },
     { location: [19.076, 72.8777], size: 0.05 },
+    { location: [40.7128, -74.006], size: 0.1 },
+    { location: [31.5204, 74.3587], size: 0.1 },
     { location: [23.8103, 90.4125], size: 0.05 },
     { location: [30.0444, 31.2357], size: 0.07 },
-    { location: [39.9042, 116.4074], size: 0.08 },
-    { location: [-23.5505, -46.6333], size: 0.05 },
     { location: [19.4326, -99.1332], size: 0.1 },
-    { location: [40.7128, -74.006], size: 0.1 },
-    { location: [34.6937, 135.5022], size: 0.05 },
     { location: [41.0082, 28.9784], size: 0.06 },
-    { location: [31.5204, 74.3587], size: 0.1 },
+    { location: [14.5995, 120.9842], size: 0.03 },
+    { location: [39.9042, 116.4074], size: 0.08 },
+    { location: [34.6937, 135.5022], size: 0.05 },
+    { location: [-23.5505, -46.6333], size: 0.05 },
   ],
 };
 
