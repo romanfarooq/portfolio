fed3141449c2306eeffeb8f083f093e2eaed5848
Roman Farooq <TheRomanFarooq@gmail.com>
2025-08-07 21:49:43 +0500

    Update CV: Adjusted employment duration for ML Bench from March 2025 – Present to March 2025 – August 2025.

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,6 +2,5 @@ export { Copy } from "./copy";
 export { Menu } from "./menu";
 export { Close } from "./close";
 export { CopyDone } from "./copy-done";
-export { ArrowUp } from "./arrow-up";
 export { ArrowDown } from "./arrow-down";
 export { ArrowRight } from "./arrow-right";
