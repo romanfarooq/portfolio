41269ffee50f9392720f2435030368871c9a83c8
Roman Farooq <TheRomanFarooq@gmail.com>
2025-10-12 21:56:10 +0500

    refactor(layout.tsx, manifest.ts): update URLs to use absolute paths for assets and metadata

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,22 +6,22 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "Roman Farooq - Portfolio",
     description:
       "Roman Farooq is an AI-focused software engineer and data science enthusiast dedicated to building intelligent, data-driven, and scalable solutions. Specializing in Python, React, Next.js, and modern machine learning integrations, Roman delivers innovative applications combining advanced analytics with user-focused design.",
-    start_url: "/",
+    start_url: "https://romanfarooq.vercel.app/",
     display: "standalone",
     background_color: "#030412",
     theme_color: "#030412",
     orientation: "portrait-primary",
-    scope: "/",
+    scope: "https://romanfarooq.vercel.app/",
     lang: "en",
     categories: ["portfolio", "developer", "technology"],
     icons: [
       {
-        src: "/favicon.ico",
+        src: "https://romanfarooq.vercel.app/favicon.ico",
         sizes: "any",
         type: "image/x-icon"
       },
       {
-        src: "/assets/images/roman.webp",
+        src: "https://romanfarooq.vercel.app/assets/images/roman.webp",
         sizes: "1200x630",
         type: "image/webp",
         purpose: "maskable"
