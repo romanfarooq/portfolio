1ba0b5dd828eb89dd6ce4da05e3d384bbd56c07c
Roman Farooq <TheRomanFarooq@gmail.com>
2025-10-16 14:50:34 +0500

    refactor(navigation.ts): add permanentRedirect to navigation exports

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -1,5 +1,11 @@
 import { routing } from "./routing";
 import { createNavigation } from "next-intl/navigation";
 
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation(routing);
+export const {
+  Link,
+  redirect,
+  useRouter,
+  usePathname,
+  getPathname,
+  permanentRedirect
+} = createNavigation(routing);
