befe2a1b4d365c91642bb6502dc764f335a297ad
Roman Farooq <theromanfarooq@gmail.com>
2025-11-11 18:28:03 +0500

    chore: update dependencies and devDependencies; refactor middleware import in proxy.ts; add type annotation in Contact.tsx

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,8 @@
-import createProxy from "next-intl/middleware";
+import createMiddleware from "next-intl/middleware";
 
 import { routing } from "@/i18n/routing";
 
-export default createProxy(routing);
+export default createMiddleware(routing);
 
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)"
