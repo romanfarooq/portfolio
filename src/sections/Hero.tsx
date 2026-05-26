15e173fd594fa504366dd834e90c2948bb8197e9
Roman Farooq <theromanfarooq@gmail.com>
2026-05-26 15:35:35 +0500

    feat: refactor SpaceStation component and update Hero section with new rotation logic

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Suspense } from "react";
-import { Float } from "@react-three/drei";
+import { MathUtils } from "three";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
+import { Float, Environment } from "@react-three/drei";
 
 import { Loader } from "@/components/Loader";
 import { HeroText } from "@/components/HeroText";
@@ -24,28 +25,43 @@ const ASTRONAUT_CONFIG: ModelConfig = {
   mobile: {
     scale: 1.2,
     position: [0.1, -1, 0],
-    rotation: [Math.PI / 6, Math.PI * 0.7, -Math.PI * 0.2]
+    rotation: [
+      MathUtils.degToRad(30),
+      MathUtils.degToRad(126),
+      MathUtils.degToRad(-36)
+    ]
   },
   desktop: {
     scale: 1.5,
     position: [1.5, -1, 0],
-    rotation: [Math.PI / 6, Math.PI * 0.7, -Math.PI * 0.2]
+    rotation: [
+      MathUtils.degToRad(30),
+      MathUtils.degToRad(126),
+      MathUtils.degToRad(-36)
+    ]
   }
 };
 
 const STATION_CONFIG: ModelConfig = {
   mobile: {
-    scale: 0.25,
-    position: [-0.72, 1.6, -1.9],
-    rotation: [Math.PI * 0.08, Math.PI * 0.35, -Math.PI * 0.08]
+    scale: 0.5,
+    position: [-0.8, 1.5, -0.85],
+    rotation: [
+      MathUtils.degToRad(38),
+      MathUtils.degToRad(-145),
+      MathUtils.degToRad(27)
+    ]
   },
   desktop: {
-    scale: 0.3,
-    position: [-1.75, 1.45, -1.9],
-    rotation: [Math.PI * 0.08, Math.PI * 0.35, -Math.PI * 0.08]
+    scale: 1,
+    position: [-2.8, 1.25, -0.85],
+    rotation: [
+      MathUtils.degToRad(38),
+      MathUtils.degToRad(-105),
+      MathUtils.degToRad(33)
+    ]
   }
 };
-
 export function Hero() {
   const isMobile = useMediaQuery({ maxWidth: 768 });
 
@@ -62,18 +78,12 @@ export function Hero() {
       <figure className="absolute inset-0 h-screen w-screen">
         <Canvas camera={{ position: [0, 1, 3] }}>
           <Suspense fallback={<Loader />}>
-            <ambientLight intensity={1.5} />
-            <pointLight position={[-5, 3, 5]} intensity={2} />
-            <directionalLight position={[5, 5, 5]} intensity={4} />
-            <Float>
-              <SpaceStation
-                {...spaceStationConfig}
-                floatBaseX={spaceStationConfig.position[0]}
-                floatBaseY={spaceStationConfig.position[1]}
-              />
-            </Float>
+            <Environment preset="city" resolution={128} />
             <Float>
               <Astronaut {...astronautConfig} />
+            </Float>
+            <Float>
+              <SpaceStation {...spaceStationConfig} />
             </Float>
           </Suspense>
         </Canvas>
